d09762e73734f6da4151d3b824863ac2aa0f9d7d
vinicius jose morais alves <vinicius.jose@gmail.com>
2021-02-23 17:27:11 -0300

    EditCompleto

--- src/app/eventos/eventoEdit/eventoEdit.component.ts
+++ src/app/eventos/eventoEdit/eventoEdit.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BsLocaleService } from 'ngx-bootstrap/datepicker';
-import { BsModalService } from 'ngx-bootstrap/modal';
 import { ToastrService } from 'ngx-toastr';
 import { Evento } from 'src/app/_models/Evento';
 import { EventoService } from 'src/app/_services/Evento.service';
-import { TabsModule } from 'ngx-bootstrap/tabs';
+import { ActivatedRoute } from '@angular/router';
+import { error } from 'console';
 
 @Component({
   selector: 'app-evento-edit',
@@ -19,10 +19,23 @@ export class EventoEditComponent implements OnInit {
   evento = {} as Evento;
   imagemURL = 'assets/img/upload.png';
   dataEvento = '';
+  file = {} as File;
+  fileNameToUpdate = '';
+
+  dataAtual = '';
+
+
+  get lotes(): FormArray {
+    return <FormArray>this.registerForm.get('lotes');
+  }
+
+  get redesSociais(): FormArray {
+    return <FormArray>this.registerForm.get('redesSociais');
+  }
 
   constructor(private eventoService: EventoService
     // tslint:disable-next-line: align
-    , private ModalService: BsModalService
+    , private router: ActivatedRoute
     // tslint:disable-next-line: align
     , private fb: FormBuilder
     // tslint:disable-next-line: align
@@ -35,16 +48,44 @@ export class EventoEditComponent implements OnInit {
 
   ngOnInit() {
     this.validation();
+    this.carregarEvento();
   }
 
+  carregarEvento(){
+    const idEvento = this.router.snapshot.params.id;
+    this.eventoService.getEventoByID(idEvento).subscribe(
+      (evento: Evento) => {
+        this.evento = Object.assign({}, evento);
+        this.fileNameToUpdate = evento.imagemURL.toString();
 
-  onFileChange(file: FileList) {
+        this.imagemURL = `http://localhost:5000/resources/images/${this.evento.imagemURL}?_ts=${this.dataAtual}`;
+
+        this.evento.imagemURL = '';
+        this.registerForm.patchValue(this.evento);
+        
+        this.evento.lotes.forEach(lote => {
+          this.lotes.push(this.criaLotes(lote));
+        })
+
+        this.evento.redeSociais.forEach(redeSocial => {
+          this.redesSociais.push(this.criaRedeSociais(redeSocial));
+        })
+
+
+      },
+      (error) => {
+        this.toastr.error(error);
+      }
+    );
+  }
+
+  onFileChange(evento: any) {
     const reader = new FileReader();
 
     reader.onload = (event: any) => this.imagemURL = event.target.result;
 
-    reader.readAsDataURL(file[0]);
-
+    this.file = evento.target.files;
+    reader.readAsDataURL(evento.target.files[0]);
   }
 
   validation() {
@@ -56,17 +97,43 @@ export class EventoEditComponent implements OnInit {
       qtdPessoas: ['', [Validators.required, Validators.max(120000), Validators.min(3)]],
       telefone: ['', [Validators.required,  Validators.maxLength(20)]],
       email: ['', [Validators.required, Validators.email, Validators.maxLength(300)]],
-      lotes: this.fb.group({
-        nome:   ['', Validators.required],
-        preco: ['', Validators.required],
-        dataInicio:  [''],
-        dataFim:  [''],
-        quantidade:  ['', Validators.required],
-      }),
-      redesSociais: this.fb.group({
-        nome: ['', Validators.required], 
-        url: ['', Validators.required], 
-      })
+      lotes: this.fb.array([]),
+      redesSociais: this.fb.array([])
+    });
+  }
+
+  criaLotes(lote: any) : FormGroup {
+    return this.fb.group({
+      id: [lote.id],
+      nome: [lote.nome, Validators.required],
+      preco: [lote.preco, Validators.required],
+      dataInicio: [lote.dataInicio],
+      dataFim: [lote.dataFim],
+      quantidade: [lote.quantidade, Validators.required],
+    });
+  }
+
+  adicionarLotes() {
+    this.lotes.push(this.criaLotes({ id: 0}));
+  }
+
+  adicionarRedesSocial() {
+    this.redesSociais.push(this.criaRedeSociais({ id: 0}));
+  }
+
+  removerLote(id: number) {
+    this.lotes.removeAt(id);
+  }
+
+  removerRedeSocial(id: number) {
+    this.redesSociais.removeAt(id);
+  }
+
+  criaRedeSociais(redeSocial: any) : FormGroup {
+    return this.fb.group({
+      id: [redeSocial.id],
+      nome: [redeSocial.nome, Validators.required], 
+      url: [redeSocial.url, Validators.required], 
     });
   }
 
